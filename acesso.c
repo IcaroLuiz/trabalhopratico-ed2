9a100a8daaa73c1c65b17f3f7b0d99ffd842a499
fxnseca <fonsecalavinia25@gmail.com>
2022-04-21 12:44:54 -0300

    acesso sequencial

--- acesso.c
+++ acesso.c
@@ -4,10 +4,12 @@
 
 void acesso_sequencial(int quantidade_itens){
     FILE *arquivo;
-    Indice *tabela = malloc(((quantidade_itens+100) / ITENSPAGINA) * sizeof(Indice));
+    int total_seila = (quantidade_itens+100) / ITENSPAGINA;
+    Indice *tabela = malloc(total_seila * sizeof(Indice));
     Registro *memoria_principal = malloc(ITENSPAGINA * sizeof(Registro));
     
     int posicao = 0, contador = 0;
+
 
     if((arquivo = fopen("arquivo_gerado.bin", "rb")) == NULL){
         printf("ERRO NA ABERTURA DO ARQUIVO!\n\n");
@@ -17,6 +19,10 @@ void acesso_sequencial(int quantidade_itens){
     while(fread(memoria_principal, sizeof(Registro), 4, arquivo) == 4){
         tabela[posicao].chave = memoria_principal[0].chave;
         posicao++;
+    }
+
+    for(int i = 0; i < total_seila;i++){
+
     }
 
 
