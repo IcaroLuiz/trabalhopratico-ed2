670537e7d380f4576d04223a132a7ff7271154e3
fxnseca <fonsecalavinia25@gmail.com>
2022-04-23 14:57:18 -0300

    continuação do acesso sequencial

--- acesso.c
+++ acesso.c
@@ -5,8 +5,33 @@
 int pesquisa_as(Indice *tabela, int tamanho, Registro *item, FILE *arquivo){
     Registro pagina[ITENSPAGINA];
     int quantidade_itens, i = 0;
-}
+    long long int deslocamento;
 
+    while(i < tamanho && tabela[i].chave <= item->chave)
+        i++;
+
+    if (i == 0)
+        return 0;
+    else{
+        fseek(arquivo, 0, SEEK_END);
+        quantidade_itens = (int) (ftello64(arquivo))/ sizeof(Registro) % ITENSPAGINA;
+
+        if(i < tamanho || quantidade_itens == 0)
+            quantidade_itens = ITENSPAGINA;
+    }
+
+    deslocamento = (i -1) * ITENSPAGINA * sizeof(Registro);
+    fseeko64(arquivo, 0, SEEK_SET);
+    fread(&pagina, sizeof(Registro), quantidade_itens, arquivo);
+    
+    for(i = 0; i < quantidade_itens; i++){
+        if(pagina[i].chave == item->chave){
+            *item = pagina[i];
+            return 1;
+        }
+    }
+    return 0;
+}
 
 void acesso_sequencial(int quantidade_itens){
     FILE *arquivo;
@@ -35,7 +60,7 @@ void acesso_sequencial(int quantidade_itens){
     print("\nDIGITE A CHAVE DE PESQUISA:");
     scanf("%d", &dado.chave);
 
-    if("função de pesquisa do acesso")
+    if(pesquisa_as(tabela, posicao,&dado, arquivo))
         printf("A CHAVE %d FOI ENCONTRADA!", dado.chave);
     else
         printf("A CHAVE %d NAO FOI ENCONTRADA!", dado.chave);
